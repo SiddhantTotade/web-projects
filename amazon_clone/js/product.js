fc43528f2b902a96d8c103d0af69987f7e69feeb
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-07-03 23:27:08 +0530

    product page - images , html, css, javascript added

---
+++ amazon_clone/js/product.js
@@ -0,0 +1,45 @@
+// Dropdown Menu
+let dropdownBtn = document.querySelector(".dropdown-btn")
+let menuContent = document.querySelector(".dropdown-menu")
+
+dropdownBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    menuContent.classList.toggle('show')
+});
+
+window.onclick = () => {
+    menuContent.classList.remove('show')
+}
+
+// Sidebar Menu
+////// const openMenu = document.querySelector('.open-menu')
+////// const closeMenu = document.querySelector('.close-menu')
+const openMenuDesktop = document.querySelector('.open-menu-desktop')
+const closeMenuDesktop = document.querySelector('.close-menu-desktop')
+
+/////// openMenu.addEventListener('click', () => {
+///////     document.querySelector('.sidebar').style.width = "250px";
+/////// });
+
+/////// closeMenu.addEventListener('click', () => {
+///////     document.querySelector('.sidebar').style.width = "0";
+/////// });
+
+openMenuDesktop.addEventListener('click', () => {
+    document.querySelector('.sidebar-desktop').style.width = "350px";
+});
+
+closeMenuDesktop.addEventListener('click', () => {
+    document.querySelector('.sidebar-desktop').style.width = "0";
+});
+
+$(function () {
+    $(".xzoom,.xzoom-gallery").xzoom({
+        zoomWidth: 600,
+        // width:"700px",
+        // height:"900em",
+        zoomHeight: 600,
+        tint: "#331",
+        Xoffset: 80,
+    })
+});
