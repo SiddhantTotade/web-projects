4f36f0ee538f6a967532fe5b31c6e488aa7b5693
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-07-11 23:16:06 +0530

    mobile cart page done

--- amazon_clone/js/cart.js
+++ amazon_clone/js/cart.js
@@ -1,24 +1,24 @@
 // Sidebar
-// const openMenu = document.querySelector('.open-menu')
-// const closeMenu = document.querySelector('.close-menu')
-// const openMenuDesktop = document.querySelector('.open-menu-desktop')
-// const closeMenuDesktop = document.querySelector('.close-menu-desktop')
+const openMenu = document.querySelector('.open-menu')
+const closeMenu = document.querySelector('.close-menu')
+const openMenuDesktop = document.querySelector('.open-menu-desktop')
+const closeMenuDesktop = document.querySelector('.close-menu-desktop')
 
-// openMenu.addEventListener('click', () => {
-//     document.querySelector('.sidebar').style.width = "250px";
-// });
+openMenu.addEventListener('click', () => {
+    document.querySelector('.sidebar').style.width = "250px";
+});
 
-// closeMenu.addEventListener('click', () => {
-//     document.querySelector('.sidebar').style.width = "0";
-// });
+closeMenu.addEventListener('click', () => {
+    document.querySelector('.sidebar').style.width = "0";
+});
 
-// openMenuDesktop.addEventListener('click', () => {
-//     document.querySelector('.sidebar-desktop').style.width = "350px";
-// });
+openMenuDesktop.addEventListener('click', () => {
+    document.querySelector('.sidebar-desktop').style.width = "350px";
+});
 
-// closeMenuDesktop.addEventListener('click', () => {
-//     document.querySelector('.sidebar-desktop').style.width = "0";
-// });
+closeMenuDesktop.addEventListener('click', () => {
+    document.querySelector('.sidebar-desktop').style.width = "0";
+});
 
 // Product Counter
 const counter = document.querySelectorAll('.product-counter');
@@ -47,7 +47,9 @@ counter.forEach((node) => {
 
 // Media Query
 if (window.screen.width >= 400) {
+    document.querySelector(".desktop").style.display = 'block'
     document.querySelector(".mobile").style.display = 'none'
+    document.querySelector(".cart-container-mob").style.display = 'none'
     document.querySelector(".upper").style.display = 'none'
     document.querySelector(".lower").style.display = 'none'
 }
