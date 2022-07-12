67bac3166182a7420aa977a650fe1f28d3b40c87
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-07-12 18:14:41 +0530

    create account page for desktop done

--- amazon_clone/js/login.js
+++ amazon_clone/js/login.js
@@ -1,6 +1,8 @@
 // Media Query
 if (window.screen.width >= 400) {
     document.querySelector(".login-desktop").style.display = 'block'
+    document.querySelector(".login-mobile").style.display = 'none'
+    document.querySelector(".delivery-footer").style.display = 'none'
 }
 else if (window.screen.width <= 400) {
     document.querySelector(".login-desktop").style.display = 'none'
