e6f7344c50cefc9a15c2bde358a286df233813da
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-07-12 16:18:31 +0530

    login and create page for mobile done

---
+++ amazon_clone/js/login.js
@@ -0,0 +1,7 @@
+if (window.screen.width >= 400) {
+    document.querySelector(".login-desktop").style.display = 'block'
+}
+else if (window.screen.width <= 400) {
+    document.querySelector(".login-desktop").style.display = 'none'
+
+}
