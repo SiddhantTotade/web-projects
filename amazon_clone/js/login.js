81d116b61d7dfe92749cc20a743cd54ab753212c
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-07-12 16:56:44 +0530

    login and create page done for mobile view

--- amazon_clone/js/login.js
+++ amazon_clone/js/login.js
@@ -1,3 +1,4 @@
+// Media Query
 if (window.screen.width >= 400) {
     document.querySelector(".login-desktop").style.display = 'block'
 }
@@ -5,3 +6,21 @@ else if (window.screen.width <= 400) {
     document.querySelector(".login-desktop").style.display = 'none'
 
 }
+
+// Login and Create
+document.getElementById('user').style.display = 'none'
+// let signin = document.getElementById('signin').checked = true;
+// let create = document.getElementById('create').checked = false;
+
+document.getElementById('signin').addEventListener('click', () => {
+    signin.checked = true
+    create.checked = false
+    document.getElementById('email').style.display = 'grid'
+    document.getElementById('user').style.display = 'none'
+})
+document.getElementById('create').addEventListener('click', () => {
+    create.checked = true
+    signin.checked = false
+    document.getElementById('user').style.display = 'block'
+    document.getElementById('email').style.display = 'none'
+})
