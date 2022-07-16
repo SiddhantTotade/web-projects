05b8e93f753f5416db7b159f9203d25652a62871
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-07-16 19:51:45 +0530

    account page done for desktop and mobile

---
+++ amazon_clone/js/account.js
@@ -0,0 +1,15 @@
+// Media Query
+if (window.screen.width >= 400) {
+    document.querySelector(".desktop").style.display = 'block'
+    document.querySelector(".mobile").style.display = 'none'
+    document.querySelector(".upper").style.display = 'none'
+    document.querySelector(".lower").style.display = 'none'
+}
+else if (window.screen.width <= 400) {
+    document.querySelector(".mobile").style.display = 'block'
+    document.querySelector(".desktop").style.display = 'none'
+    document.querySelector(".user-account").style.display = 'none'
+    document.querySelector(".upper-desk").style.display = 'none'
+    document.querySelector(".middle-desk").style.display = 'none'
+    document.querySelector(".lower-desk").style.display = 'none'
+}
