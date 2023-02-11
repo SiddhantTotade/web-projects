524cfd6294571c790d2f70f0e8f4d5fefa1ab90f
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-02-11 21:30:09 +0530

    done

--- navbar-toggle/script.js
+++ navbar-toggle/script.js
@@ -1,33 +1,23 @@
 let lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, cupiditate architecto, dolor a nesciunt similique eos adipisci voluptatum hic laborum unde! Dolores sequi deleniti laudantium nostrum earum, velit exercitationem. Odit iure in vitae architecto corporis recusandae saepe tempore modi aliquid iustoexercitationem ipsam adipisci suscipit, illum aliquam quae, assumenda nesciunt vero enim necessitatibusillo, tenetur minima! Iure repudiandae tempore ratione culpa voluptatibus hic ipsam enim tenetur istevoluptatum ea nisi consequatur eum, quasi deleniti quisquam fugiat et quidem odit? Dolore praesentiumtemporibus quas doloremque minus laboriosam dicta possimus vero inventore placeat, molestias aperiam quosquod iusto esse voluptatum voluptatibus. Officia'
 
-document.getElementById('home').addEventListener('click', () => {
-    let active = document.getElementById('content__div')
-    if (active.classList.contains('active')) {
-        return
-    }
-    else {
-        active.classList.add('active')
-        let content_div = document.getElementById('content__div')
-        let home_content = document.createTextNode(lorem)
-        content_div.innerHTML += '<h1>Home</h1>'
-        content_div.appendChild(home_content)
-    }
+const home = document.getElementById('home')
+const contact = document.getElementById('contact')
+const about = document.getElementById('about')
+
+const title = document.getElementById('content__title')
+const para = document.getElementById('content__para')
+
+home.addEventListener('click', () => {
+    title.innerHTML = "<u>Home</u>"
+    para.innerHTML = lorem
 })
 
-document.getElementById('about').addEventListener('click', () => {
-    let active = document.getElementById('content__div')
-    if (active.classList.contains('active')) {
-        return
-    }
-    else {
-        active.classList.add('active')
-        let content_div = document.getElementById('content__div')
-        let home_content = document.createTextNode(lorem)
-        content_div.innerHTML += '<h1>Contact</h1>'
-        content_div.appendChild(home_content)
-    }
+contact.addEventListener('click', () => {
+    title.innerHTML = "<u>Contact</u>"
+    para.innerHTML = lorem
 })
 
-document.getElementById('contact').addEventListener('click', () => {
-    document.getElementById('content__div').innerHTML += "<h1>Contact</h1>" + "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, cupiditate architecto, dolor a nesciunt similique eos adipisci voluptatum hic laborum unde! Dolores sequi deleniti laudantium nostrum earum, velit exercitationem. Odit iure in vitae architecto corporis recusandae saepe tempore modi aliquid iustoexercitationem ipsam adipisci suscipit, illum aliquam quae, assumenda nesciunt vero enim necessitatibusillo, tenetur minima! Iure repudiandae tempore ratione culpa voluptatibus hic ipsam enim tenetur istevoluptatum ea nisi consequatur eum, quasi deleniti quisquam fugiat et quidem odit? Dolore praesentiumtemporibus quas doloremque minus laboriosam dicta possimus vero inventore placeat, molestias aperiam quosquod iusto esse voluptatum voluptatibus. Officia"
+about.addEventListener('click', () => {
+    title.innerHTML = "<u>About</u>"
+    para.innerHTML = lorem
 })
