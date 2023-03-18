8915a57e83437df02b60fcee40f408b8f95bf6f6
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-03-18 18:25:48 +0530

    modified : App.css

--- inc_dec_counter_react_redux/src/App.js
+++ inc_dec_counter_react_redux/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div class="container">
+      <h1>Increment Decrement Counter App</h1>
+      <div class="quantity">
+        <button href="/" class="quantity__minus" title="decrement">
+          <span>-</span>
+        </button>
+        <input name="quantity" type="text" class="quantity__input" value="0" />
+        <button href="/" class="quantity__plus" title="increment">
+          <span>+</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
