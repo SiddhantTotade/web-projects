599352268d6ce8328750a649441807426982aba6
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-09-22 00:37:40 +0530

    initial commit : new project - car showcase

---
+++ car_showcase/components/CustomButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import react from "react";
+import image from "next/image";
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
